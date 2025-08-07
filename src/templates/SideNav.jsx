e52a79fff7dfe5dc6ebd7d7686a5eb9c7a0e4714
Abhinav-Anil-5670 <abhinavanil800@gmail.com>
2025-08-07 21:56:25 +0530

    side_nav_initial

--- src/templates/SideNav.jsx
+++ src/templates/SideNav.jsx
@@ -8,9 +8,20 @@ const SideNav = () => {
             <i class=" text-[#6556CD] ri-tv-fill mr-2 "></i>
             <span >Movie APP</span>
         </h1>
-        <nav>
+        <nav className='flex flex-col text-zinc-400 text-xl gap-3'>
             <h1 className='text-white font-semibold text-xl mt-10 mb-5'>New Feeds</h1>
-            <Link></Link>
+            <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5 '><i class="ri-fire-line"></i>Trending</Link>
+            <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5 '><i class="ri-bard-line"></i> Popular</Link>
+            <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5 '><i class="ri-movie-line"></i> Movies</Link>
+            <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5 '><i class="ri-tv-2-line"></i> Tv Shows</Link>
+            <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5 '><i class="ri-team-fill"></i> People</Link>
+        </nav>
+        <hr className='border-none h-[1px] bg-zinc-400' />
+        <nav className='flex flex-col text-zinc-400 text-xl gap-3'>
+            <h1 className='text-white font-semibold text-xl mt-10 mb-5'>Website Information</h1>
+            <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5 '><i class="ri-fire-line"></i> About CineVault</Link>
+            <Link className='hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5 '><i class="ri-information-line"></i> Contact US</Link>
+            
         </nav>
     </div>
   )
