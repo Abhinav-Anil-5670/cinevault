c3692c80628976f8e10f3005c1e03af800909677
Abhinav-Anil-5670 <abhinavanil800@gmail.com>
2025-08-07 20:54:39 +0530

    Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import SideNav from '../templates/SideNav'
 
 const Home = () => {
-    document.title='Home'
+    document.title='Cinevault | Home'
   return (
     
     <>
       <SideNav/>
-      <div className='w-[80%] h-full bg-blue'></div>
+      <div className='w-[80%] h-full'></div>
     </>
   )
 }
--- src/templates/SideNav.jsx
+++ src/templates/SideNav.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SideNav = () => {
   return (
-    <div className='w-[20%] h-full border-r-2 border-zinc-200 bg-red'>
+    <div className='w-[20%] h-full border-r-2 border-zinc-200 p-10 '>
         <h1 className='text-2xl text-[white] font-bold  '>
             <i class=" text-[#6556CD] ri-tv-fill mr-2 "></i>
             <span >Movie APP</span>
         </h1>
+        <nav>
+            <h1 className='text-white font-semibold text-xl mt-10 mb-5'>New Feeds</h1>
+            <Link></Link>
+        </nav>
     </div>
   )
 }
