91d52a6e6718de551bbbfb2760852eb14d6792ab
Abhinav-Anil-5670 <abhinavanil800@gmail.com>
2025-08-08 22:51:51 +0530

    Search_Function

--- src/templates/TopNav.jsx
+++ src/templates/TopNav.jsx
@@ -1,20 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from '../utils/axios'
+import noimage from '/noimage.jpg'
 
 
 const TopNav = () => {
     const [query, setquery] = useState("")
+    const [search, setsearch] = useState(null)
+    const getSearch = async ()=>{
+        try{
+            const {data} = await axios.get(`/search/multi?query=${query}`)
+            console.log(data.results)
+            setsearch(data.results)
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+    useEffect(()=>{
+        getSearch()
+    },[query])
+    
   return (
     <div className='w-full h-[10vh] relative flex justify-start items-center ml-[20%]'>
         <i class="ri-search-line text-zinc-400 text-3xl"></i>
         <input type="text" className='w-[50%] mx-10 p-5 text-xl outline:none border:none bg-transparent text-white' placeholder='Search Anything' onChange={(e)=>setquery(e.target.value)} value={query}/>
         {query.length > 0 && <i onClick={()=>setquery("")} class="ri-close-line text-zinc-400 text-3xl"></i>}
 
-        <div className='absolute w-[50%] rounded max-h-[50vh] bg-zinc-300  top-[90%] overflow-auto'>
-            {/* <Link className='p-10 bg-zinc-200 w-full flex justify-start items-center border-b-2 border-zinc-100 text-zinc-400 font-semibold hover:text-black hover:bg-zinc-300 duration-300'>
-                <img src="" alt="" />
-                <span>Hello</span>
-            </Link> */}
+        <div className='absolute w-[50%] rounded max-h-[50vh] bg-zinc-300  top-[90%] overflow-auto '>
+            {search?.map((s,i)=>{
+                return <Link key={i} className='p-10 bg-zinc-200 w-full flex justify-start items-center border-b-2 border-zinc-100 text-zinc-400 font-semibold hover:text-black hover:bg-zinc-300 duration-300'>
+                <img className='w-[10vh] h-[15vh] object-cover rounded mr-10' src={ s.poster_path || s.profile_path ? `https://image.tmdb.org/t/p/original/${s.poster_path || s.profile_path}`:noimage} alt="" />
+                <span>{s.title || s.name || s.original_title || s.original_name}</span>
+            </Link>
+            })}
+            {/*  */}
 
             
         </div>
