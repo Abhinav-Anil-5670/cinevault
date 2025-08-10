401a732c0c6c98bdb0be093f766e1ad2fb7db2fa
Abhinav-Anil-5670 <abhinavanil800@gmail.com>
2025-08-10 21:07:42 +0530

    fixed_card_and_home_page_completed

--- src/templates/Card.jsx
+++ src/templates/Card.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Card = ({ data, title }) => {
   console.log(data);
   return (
-    <div className="flex flex-wrap w-full h-full px-[5%] bg-[#1F1E24]">
+    <div className=" flex flex-wrap w-full h-full px-[5%] bg-[#1F1E24]">
       {data.map((c, i) => {
         return (
-          <Link className="w-[25vh] mr-[5%] mb-[5%]" key={i}>
+          <Link className="relative w-[25vh] mr-[5%] mb-[5%]" key={i}>
             <img
               className="w-[40vh] h-[40vh] object-cover shadow-[8px_17px_38px_2px_rgba(0,0,0,.5)] "
               src={`https://image.tmdb.org/t/p/original/${
@@ -15,7 +15,9 @@ const Card = ({ data, title }) => {
               })`}
               alt=""
             />
-            <h1 className="text-2xl mt-3 text-zinc-400">{c.title || c.name || c.original_name || c.original_title}</h1>
+            <h1 className=" mt-3 text-zinc-400 text-xl font-semibold">{c.title || c.name || c.original_name || c.original_title}</h1>
+            {c.vote_average && <div className="absolute right-[-10%] bottom-[25%] rounded-full bg-yellow-600 text-white w-[5vh] h-[5vh] flex justify-center items-center">{(c.vote_average*10).toFixed()} <sup>%</sup></div>
+}
           </Link>
         );
       })}
